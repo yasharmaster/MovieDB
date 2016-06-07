bce8b7dace434473e59f2bded23d705263b1074e
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-07 18:20:13 +0000

    Add new and create actions & view files for Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,20 @@
 class MoviesController < ApplicationController
+    
     def index
-        
+    
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.new(movie_params)
+    end
+    
+    private
+    
+    def movie_params
+       params.require(:movie).permit(:title, :description, :director, :release_date) 
     end
 end
