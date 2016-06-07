3297e9fa5d1cd3396b18686a7929d275a4c0fcc7
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-07 17:32:54 +0000

    movies model & controller added

---
+++ db/migrate/20160607165154_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :director
+      t.date :release_date
+
+      t.timestamps null: false
+    end
+  end
+end
