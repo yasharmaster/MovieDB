441c0c8e268875cffe0d1f677b1449bf03584da9
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-08 02:43:42 +0000

    Add show & new views and action for Movies

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MovieDB
-=========
-A movies portal like _broadway_ & _imdb_.
+----------
+A portal for movie lovers where you can rate your favourite movies and list movies for watching in categories. Sort of like **_Imdb_** and **_Broadway_**.
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
     
-    def index
+    before_action :find_movie, only: [:show, :edit, :update, :destroy]
     
+    def index
+        @movies = Movie.all.order("created_at DESC")
     end
     
     def new
@@ -10,11 +12,25 @@ class MoviesController < ApplicationController
     
     def create
         @movie = Movie.new(movie_params)
+        
+        if @movie.save
+            redirect_to root_path
+        else
+            render new
+        end
+    end
+    
+    def show
+        
     end
     
     private
     
     def movie_params
        params.require(:movie).permit(:title, :description, :director, :release_date) 
+    end
+    
+    def find_movie
+       @movie = Movie.find(params[:id]) 
     end
 end
