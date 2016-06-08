b97efa24b3b70a2063b26fda52dd01c8b400a86c
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-08 14:33:11 +0000

    Add category filtering on index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,13 @@ class MoviesController < ApplicationController
     before_action :find_movie, only: [:show, :edit, :update, :destroy]
     
     def index
-        @movies = Movie.all.order("created_at DESC")
+        if params[:category].blank?
+            @movies = Movie.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @movies = Movie.where(:category_id => @category_id).order("created_at DESC")
+        end
+
     end
     
     def new
