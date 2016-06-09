01204bbd051ad80bafedcc572cd9d8130acbc49f
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-09 07:51:45 +0000

    Add reviews model, migrations and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
+    has_many :reviews
     has_attached_file :movie_img, styles: { :movie_index => "250x350>", :movie_show => "325x475>" }, default_url: "/missing.png"
     validates_attachment_content_type :movie_img, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609052426) do
+ActiveRecord::Schema.define(version: 20160609074801) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20160609052426) do
     t.string   "movie_img_content_type"
     t.integer  "movie_img_file_size"
     t.datetime "movie_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
