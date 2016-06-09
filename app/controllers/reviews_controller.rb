6681393386cc0ae379de6b26e3d816d1981321c1
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-09 13:43:38 +0000

    Add edit & delete review views & actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
-    before_action :find_movie, only: [:new, :create]
+    before_action :find_movie
     before_action :authenticate_user!, only: [:new, :create]
+    before_action :find_review, only: [:edit, :update, :destroy]
     
     def new
         @review = Review.new
@@ -18,6 +19,22 @@ class ReviewsController < ApplicationController
         end
     end
     
+    def edit
+    end
+    
+    def update
+        if @review.update(review_params)
+            redirect_to movie_path(@movie)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @review.destroy
+        redirect_to movie_path(@movie)
+    end
+    
     private
         def review_params
             params.require(:review).permit(:rating, :comment)
@@ -25,5 +42,9 @@ class ReviewsController < ApplicationController
         
         def find_movie
             @movie = Movie.find(params[:movie_id])
+        end
+        
+        def find_review
+            @review = Review.find(params[:id])
         end
 end
