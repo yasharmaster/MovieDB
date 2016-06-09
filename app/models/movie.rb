250bd036ecec209b5421ea950eae15a729ded961
Yash Sharma <ihm2014006@iiita.ac.in>
2016-06-09 05:49:37 +0000

    Render images on index page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
-    has_attached_file :movie_img, styles: { :movie_index => "250x350>", :movie_show => "325x475>" }, default_url: "/images/missing.png"
+    has_attached_file :movie_img, styles: { :movie_index => "250x350>", :movie_show => "325x475>" }, default_url: "/missing.png"
     validates_attachment_content_type :movie_img, content_type: /\Aimage\/.*\Z/
 end
